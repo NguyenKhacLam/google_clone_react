884a3bcf2aed8f2455f567f0714431252694a75b
Nguyen Khac Lam <khaclam2409@gmail.com>
2020-08-21 12:55:01 +0700

    don home ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles/App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Google Clone</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/search">
+            <h1>Search Page</h1>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
